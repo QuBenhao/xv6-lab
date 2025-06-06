d84189f3156573594b96bcbd10fd38a50a74232d
qubh <qubenhao2@gmail.com>
2025-06-06 15:58:23 +0800

    feat: trace solution

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -91,3 +91,13 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+uint64
+sys_trace(void)
+{
+  struct proc *p = myproc();
+  int mask;
+  argint(0, &mask);
+  p->trace_mask = mask;
+  return 0;
+}
